b61bac40a6a7d4a39cd0d1c0e0dd94a3ad6d1c7c
Nikolay Chereliuk <kolyanerushaev@gmail.com>
2023-01-10 22:04:23 +0200

    done

--- app.js
+++ app.js
@@ -10,12 +10,13 @@ const app = express()
 
 const formatsLogger = app.get('env') === 'development' ? 'dev' : 'short'
 
-app.use(logger(formatsLogger))
-app.use(cors())
-app.use(express.json())
+app.use(logger(formatsLogger));
+app.use(cors());
+app.use(express.json());
+app.use(express.static("public"));
 
-app.use('/api/auth', authRouter)
-app.use('/api/contacts', contactsRouter)
+app.use('/api/auth', authRouter);
+app.use('/api/contacts', contactsRouter);
 
 app.use((req, res) => {
   res.status(404).json({ message: 'Not found' })
